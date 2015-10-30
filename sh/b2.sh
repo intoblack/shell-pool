961278a80641235ee620752d7caab16dbacb9761
intohole <intoblack86@gmail.com>
2015-10-31 01:20:42 +0800

    1. 提交一个解压脚本 ， 方便自己处理数据

--- sh/b2.sh
+++ sh/b2.sh
@@ -85,3 +85,31 @@ BEGIN{
         }
 }' | sort -k 1 
 }
+
+
+
+function untar(){
+
+    if [ -f "$1" ];then
+        local un_tar_command=`echo "$1"  | awk '{
+        if($1 ~/\.tgz$/){
+                print "tar -zxvf"
+        }else if($1 ~/\.tar\.gz$/){
+            print "tar -xzvf"
+        }else if($1 ~/\.tar$/){
+            print "tar -xvf"
+        }else if($1 ~/\.tar\.bz2$/){
+            print "tar -xjvf";
+        }else if($1 ~/\.tar\.Z$/){
+            print "tar -xZvf" ;
+        }else if($1 ~/\.rar$/){
+            print "unrar e"
+        }else if($1 ~/\.zip$/){
+            print "unzip"
+        }
+    }'`
+        ${un_tar_command} $1
+    else
+       echo "$1 not exist file , please check"
+    fi
+                                                                    }
