83ca27474b0af1a958c4be363741f6ec43e9b47d
intohole <intoblack86@gmail.com>
2016-03-07 00:55:31 +0800

    没有完成脚本 ， 继续更新hosts

---
+++ sh/update_hosts.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+
+
+
+
+wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -qO /tmp/hosts 
+[ $? -ne 0 ] && exit 1
+mkdir /etc/hosts_bak 
+cp /etc/hosts /etc/hosts_bak/hosts_`date '+%Y%m%d'` 
+gawk 'ARGIND==1{
+        if($0 ~/^#/ || NF != 2){
+                next ;
+        }
+        old_hosts[$2] = $1
+    } 
+    ARGIND==2{
+        if($0 ~/^#/ || NF !=2 ){
+            next ;
+        }
+        new_hosts[$2] = $1
+    } END{
+        for(host in old_hosts){
+            if(host in new_hosts){
+                print new_hosts[host] , host 
+            }else{
+                print old_hosts[host] , host
+            }
+        }
+
+        for(host in new_hosts){
+           if(!(host in old_hosts)){
+               print old_hosts[host] , host
+           }
+        }        
+}' /etc/hosts /tmp/hosts 
+
